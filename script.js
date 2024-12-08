25c5cd7fb083d08c57f0a8e74fa69b06dbf5bd92
Aarya Thakur <aaryathakur1704@gmail.com>
2024-12-09 02:36:18 +0530

    create an unordered list. Allow users to add and remove list item dinamically using button.

--- script.js
+++ script.js
@@ -1,78 +1,24 @@
-//  1
-// var btn = document.querySelector("button");
-// var p = document.querySelector("p");
-// btn.addEventListener("click", function(){
-//     p.textContent = 'hey hello';
-// })
+var add = document.querySelector("#add");
+var remove = document.querySelector("#remove");
+var inp = document.querySelector("input");
+var ul = document.querySelector("ul");
 
-// 2
 
-// var img1 = document.querySelector("#one");
-// var img1 = document.querySelector("#two");
+var li;
 
-// document.querySelector("button")
-// .addEventListener("click", function(){
-//     var src1 =img1.src;
-//     var src2 = img2.src;
+add.addEventListener("click", function(){
+    if(inp.value.trim() === ""){
 
-//     img1.src= src2;
-//     img2.src = src1;
+    } else{
+        li = document.createElement("li")
+        li.textContent = inp.value;
+        ul.appendChild(li);
+        inp.value = "";
+        
+    }
+    
+})
 
-// })
-
-// 3
-
-// var form = document.querySelector("form");
-// var inp1 = document.querySelector("#inp1");
-// var inp2 = document.querySelector("#inp2");
-// var h4 = document.querySelector("h4")
-
-// form.addEventListener("submit" , function(ev){
-//     ev.preventDefault();
-//     if(inp1.value === '' || inp2.value === ''){
-//         h4.textContent = "error, some fields are blank";
-//         h4.style.color = "red";
-//     }
-//     else{
-//         h4.textContent="";
-//         h4.style.color="black";
-//     }
-// })
-
-// or we can also write for not giving too much input
-
-// var form = document.querySelector("form");
-// var inps = document.querySelectorAll('input[type="text"]');
-// var h4 = document.querySelector("h4");
-
-// form.addEventListener("submit", function (ev) {
-//     ev.preventDefault();
-//     inps.forEach(function (inp) {
-//         if (inp.value === '') {
-//             h4.textContent = 'Error,some fields are empty';
-//             h4.style.color = "red";
-//         }
-//         else {
-//             h4.textContent = '';
-//             h4.style.color = "black";
-//         }
-
-//     })
-// })
-
-// or also we can solve like
-
-// var form = document.querySelector("form");
-// var inps = document.querySelectorAll('input[type="text"]');
-// var h4 = document.querySelector("h4");
-
-// form.addEventListener("submit", function (ev) {
-//     ev.preventDefault();
-//     for (var i = 0; i < inps.length; i++) {
-//         if (inps[i].value.trim() === '') {
-//             h4.textContent = "Error, some fields are blank";
-//             h4.style.color = "red";
-//             break;
-//         }
-//     }
-// })
+remove.addEventListener("click", function(){
+    ul.removeChild(li);
+})
